daceb85adaa92c21b3ced147695af4e0416ebc3d
rony derra <derrarony@gmail.com>
2022-12-26 19:49:15 +0200

    PaymentForm

--- src/components/PaymentForm.js
+++ src/components/PaymentForm.js
@@ -1,64 +1,46 @@
-import * as React from 'react';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
+import * as React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
 
 export default function PaymentForm() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  async function getData() {
+    const data = await axios.get("https://xp-challenges.herokuapp.com/getAllAchievments");
+    console.log(data.data);
+    setData(data.data);
+  }
+
   return (
     <React.Fragment>
       <Typography variant="h6" gutterBottom>
         Payment method
       </Typography>
       <Grid container spacing={3}>
-        <Grid item xs={12} md={6}>
-          <TextField
-            required
-            id="cardName"
-            label="Name on card"
-            fullWidth
-            autoComplete="cc-name"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            required
-            id="cardNumber"
-            label="Card number"
-            fullWidth
-            autoComplete="cc-number"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            required
-            id="expDate"
-            label="Expiry date"
-            fullWidth
-            autoComplete="cc-exp"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            required
-            id="cvv"
-            label="CVV"
-            helperText="Last three digits on signature strip"
-            fullWidth
-            autoComplete="cc-csc"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <FormControlLabel
-            control={<Checkbox color="secondary" name="saveCard" value="yes" />}
-            label="Remember credit card details for next time"
-          />
-        </Grid>
+        {data.map((item, index) => {
+          return (
+            <Grid item xs={12} md={6} key={index}>
+              <TextField
+                required
+                id={item.achievmentNumber}
+                label={item.name}
+                helperText={item.description}
+                fullWidth
+                autoComplete="cc-csc"
+                variant="standard"
+              />
+            </Grid>
+          );
+        })}
       </Grid>
     </React.Fragment>
   );
